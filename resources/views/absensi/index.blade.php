ee377d5e2ec8ccec84fcb1a732805872c7017c16
pineappledust <gameakunjon@gmail.com>
2026-06-15 05:48:53 +0700

    hapus tombol edit dan delete halaman index absensi

--- resources/views/absensi/index.blade.php
+++ resources/views/absensi/index.blade.php
@@ -112,7 +112,6 @@
                             <th class="p-3 text-left text-teal-800 font-semibold border-b border-teal-200 text-sm">Scan (In - Out)</th>
                             <th class="p-3 text-left text-teal-800 font-semibold border-b border-teal-200 text-sm">Status</th>
                             <th class="p-3 text-left text-teal-800 font-semibold border-b border-teal-200 text-sm">Potongan</th>
-                            <th class="p-3 text-center text-teal-800 font-semibold border-b border-teal-200 text-sm">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -159,24 +158,7 @@
                                 {{ $absensis->potongan_jam }} Jam
                             </td>
 
-                            <td class="p-3 border-b border-gray-200">
-                                <div class="flex justify-center space-x-3">
-                                    <a href="{{ route('absensi.edit', $absensis->id) }}" class="text-teal-600 hover:text-teal-800 transition-colors" title="Edit">
-                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-                                        </svg>
-                                    </a>
-                                    <form action="{{ route('absensi.destroy', $absensis->id) }}" method="POST" onsubmit="return confirm('Yakin ingin dihapus?')">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-600 hover:text-red-800 transition-colors" title="Hapus">
-                                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5-4h4m-4 0h4m-7 4h10"></path>
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
+
                         </tr>
                         @empty
                         <tr>
